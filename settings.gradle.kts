382ab0d0ab26e3390ae7b7d64aea829d32c31b7d
student <student@gnu.ac.in>
2023-08-18 08:51:56 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "21012021011_Practical-5_MAD"
+include(":app")
